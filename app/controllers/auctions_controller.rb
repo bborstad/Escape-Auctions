6f320f7096a315ecfbc990fee19c8a69cbf041d3
Benjamin Borstad <bborstad@gmail.com>
2020-04-23 18:23:49 -0500

    WIP: show page created. Need to add search feature

--- app/controllers/auctions_controller.rb
+++ app/controllers/auctions_controller.rb
@@ -5,4 +5,14 @@ class AuctionsController < ApplicationController
             format.html { render :index, locals: {auctions: auctions } }
         end
     end
+
+    def show
+        auction = Auction.find(params[:id])
+        respond_to do |format|
+            format.html { render :show, locals: { auction: auction } }
+        end
+    end
+
+    def search
+    end
 end
