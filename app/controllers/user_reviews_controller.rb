56eaa8ab1c74816a9f1b259651a862ef8ff9024a
Corey Bedell <cebedell@outlook.com>
2020-04-24 16:52:58 -0500

    Pushing changes to review submission form

--- app/controllers/user_reviews_controller.rb
+++ app/controllers/user_reviews_controller.rb
@@ -8,14 +8,28 @@ class UserReviewsController < ApplicationController
     end
     
     def submission
+        required = [:name, :email, :reply, :message]
+        form_complete = true
+        required.each do |f|
+            if params.has_key? f and not params[f].blank?
+     # that's good news. do nothing
+            else
+                form_complete = false
+            end
+        end
+        if form_complete
+            form_status_msg = 'Thank you for your review.'
+        else    
+            form_status_msg = 'Please fill in all fields and resubmit.'
+        end
         respond_to do |format|
-            format.html {render :submission}
+            format.html {render :update, locals: {status_msg: form_status_msg, feedback: params}}
         end
     end
 
     def update
         respond_to do |format|
-            format.html {render :submission}
+            format.html { render :update, locals: {feedback: {} } }
         end
     end
 
