0c8f85727fe9dc22a6daa94cf2c6330855cee281
Corey Bedell <cebedell@outlook.com>
2020-04-27 19:28:30 -0500

    updated review functionality to tie directly back to users and only allow specific user to deleted posted review

--- app/models/auction.rb
+++ app/models/auction.rb
@@ -30,13 +30,7 @@ class Auction < ApplicationRecord
         foreign_key: 'user_id',
         inverse_of: :auctions
     )
-    has_many(
-        :reviews,
-        class_name: 'Reviews',
-        foreign_key: 'auction_id',
-        inverse_of: :auctions,
-        dependent: :destroy
-    )
+    has_many :reviews
 
     acts_as_taggable_on :tags
 
